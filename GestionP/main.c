99982daee40072d052fe1c16c5835218157b2580
boodah <boodah21@protonmail.com>
2019-04-30 00:54:56 -0500

    Se lee carga completa. Descripcion corregido.

--- GestionP/main.c
+++ GestionP/main.c
@@ -21,14 +21,14 @@ void carga_data();
 
 int main(int argc, char const *argv[])
 {
-    char pass[4];int i=0;
+    char pass[4];int des=0;char *temp=carga_memo_char(temp,200);
     system("clear");
     printf("Contrasena: ");printf(KGRN);scanf("%[^\n]",pass);printf(KNRM);
-
+        
     if(strcmp(pass,"1q2w")==0){
         system("clear");
-        puts("Iniciando una nueva carga de actividades:\n");
-        for(int i = 0; i < 40; i++)
+        puts("Iniciando una nueva carga de actividades:");
+        for(int i = 0; i < 41; i++)
         {
             fflush(stdout);
             printf(KGRN);printf("+");printf(KNRM);
@@ -37,31 +37,30 @@ int main(int argc, char const *argv[])
         printf("\n\n\n");
         //aqui debe ir metodos de apertura de archivo.
 
-        //cargo en memoria
-        d.nom_act=carga_memo_char(d.nom_act,140);
-        
+        d.nom_act=carga_memo_char(d.nom_act,60);
+        //VALIDAR QUE NINGUN CAMPO SEA NULL o SIN VALOR 
         printf("Nombre de la actividad: ");printf(KGRN);scanf("%s",d.nom_act);printf(KNRM);
-        printf("\n");
-        d.desc_act=carga_memo_char(d.desc_act,256);
-        fflush( stdin ); char *temp = carga_memo_char(temp,256);
-        printf("Descripcion de la actividad: ");printf(KGRN);scanf("%[^\n]",temp);printf(KNRM);
-        strcpy(d.desc_act,temp);
+        fflush( stdin );
+        printf("Desea agregar una descripcion? 0/1 =  ");printf(KGRN);scanf("%d",&des);printf(KNRM);
         fflush(stdin);
-        printf("\n");
+            if(des==0){
+                d.desc_act=carga_memo_char(d.desc_act,200);
+                printf("Descripcion de la actividad: ");
+                scanf(" %[^\n]",temp);
+                fflush(stdin);
+                strcpy(d.desc_act, temp);
+                free(temp);temp=NULL;
+            }//edn if
        //dar mas control a las fechas, validar si inicia justo el dia de la carga
-    printf(KYEL);printf("\n\nEL FORMATO DE FECHA ES: AAAA/MM/DD * Esto como obligatorio.\n\n");printf(KNRM);
+    printf(KYEL);printf("\n*EL FORMATO DE FECHA ES: AAAA/MM/DD * Esto como obligatorio.\n");printf(KNRM);
         printf("Fecha de inicio de la actividad: ");printf(KGRN);scanf("%s",d.f_inicio);printf(KNRM);
-        printf("\n");
+        fflush( stdin );
         printf("Fecha de finalizacion de la actividad: ");printf(KGRN);scanf("%s",d.f_final);printf(KNRM);
-        printf("\n");
+        fflush( stdin );
         //y la finalizacion se puede disparar en otro hilo de ejecucion que espere la finalizacion de la actividad, como especie de contador.
-    printf(KYEL);printf("\n\nLA PRIORIDAD SE ESTABLECE POR A(alta), B(media), C(baja), EN MAYUSCULAS. Esto como obligatorio.\n\n");printf(KNRM);
+    printf(KYEL);printf("\n*LA PRIORIDAD SE ESTABLECE POR A(alta), B(media), C(baja), EN MAYUSCULAS. Esto como obligatorio.\n");printf(KNRM);
         printf("Prioridad de la actividad: "); printf(KGRN);scanf("%s",d.prioridad);printf(KNRM);
-
-
-
-
-
+        fflush( stdin );
 
     //AQUI DEBO DE CARGAR TODO Y GUARDAR ARCHIVO
     }else{
