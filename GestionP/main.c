d9deb8c5e70159178ab7ba8718d14f4b0d1ec982
boodah <boodah21@protonmail.com>
2019-04-29 20:49:53 -0500

    Se crea una nueva entrada de proyecto personal.

---
+++ GestionP/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    char pass[6];
+    system("clear");
+    printf("\n\t\tBienvenidos al gestion de proyectosV1.0\n\n\n");
+
+    printf("Contrasena: ");scanf("%[^\n]",pass);
+    if(strcmp(pass,"1q2w")){
+        /*Metodo de inicio para Beatriz*/
+    }
+    return EXIT_SUCCESS;
+}
