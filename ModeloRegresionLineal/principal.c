c74bfe6a2f8a18d6cf360f074d4bca86be7f8a40
boodah <boodah21@protonmail.com>
2019-05-14 00:09:28 -0500

    Se culmina el modelo de regresion lineal.

--- ModeloRegresionLineal/principal.c
+++ ModeloRegresionLineal/principal.c
@@ -9,8 +9,8 @@
 #define KRED  "\x1B[31m"
 #define KMAG  "\x1B[35m"
 
-int cal_b(int *, int *, int, int, int);
-int cal_r(int *, int *, int, int, int);
+float cal_b(float *, float *, float, float, float);
+float cal_r(float *, float *, float, float, float);
 
 int main(int argc, char const *argv[])
 {
@@ -19,7 +19,7 @@ int main(int argc, char const *argv[])
 	printf("%s\nMODELO REGRESION LINEAL%s\n\n\n",KYEL,KNRM);
 
 	printf("Cual es la cantidad de datos a leer: ");printf(KGRN);scanf("%d",&n);printf(KNRM);
-	int x[n], y[n],result, id=0, sumx=0, sumy=0; float x1=0,y1=0,a,b,R=0;
+	float result, id=0, sumx=0, sumy=0; float x[n], y[n],x1=0,y1=0,a=0,b=0,R=0;
 	printf("Iniciando carga de datos\n");
 	////////////////////////////////////////////////////
 	    for(int x = 0; x < 41; x++)
@@ -32,10 +32,10 @@ int main(int argc, char const *argv[])
     ///////////////////////////////////////////////////
 		for (int i = 0; i < n; i++)
 		{
-			printf("X[%d] = ",i+1);printf(KGRN);scanf("%d",&x[i]);printf(KNRM);	
+			printf("X[%d] = ",i+1);printf(KGRN);scanf("%f",&x[i]);printf(KNRM);	
 				sumx += x[i];
 				//x1 += (x[i]/n);	
-			printf("Y[%d] = ",i+1);printf(KGRN);scanf("%d",&y[i]);printf(KNRM);	
+			printf("Y[%d] = ",i+1);printf(KGRN);scanf("%f",&y[i]);printf(KNRM);	
 				sumy += y[i];
 				//y1 += (y[i]/n);	
 			id++;	
@@ -46,17 +46,17 @@ int main(int argc, char const *argv[])
 		printf("EL VALOR DE X^ = %s %f %s \n",KRED,x1,KNRM);
 		printf("EL VALOR DE Y^ = %s %f %s \n\n",KRED,y1,KNRM);
 		b = cal_b(x,y,x1,y1,n);
-		a = y1 - (b * sumx);
-		printf("EL VALOR DE B = %s %f %s \n\n",KRED,b,KNRM);
+		a = (y1 - (b * x1));
+		printf("EL VALOR DE B = %s %f %s \n",KRED,b,KNRM);
 		printf("EL VALOR DE A = %s %f %s \n\n",KRED,a,KNRM);
 		R = cal_r(x,y,x1,y1,n);
-		printf("\n\nEL VALOR DE R = %s %f %s \n\n",KMAG,a,KNRM);
+		printf("\n\nEL VALOR DE R = %s %f %s \n\n",KMAG,R,KNRM);
 
 
 	return 0;
 }
-int cal_b(int *x, int *y, int x1, int y1, int n){
-	int s1=0,s2=0;
+float cal_b(float *x, float *y, float x1, float y1, float n){
+	float s1=0,s2=0;
 	for (int i = 0; i < n; i++)
 	{
 		s1 += (x[i] - x1)*(y[i]-y1);
@@ -66,12 +66,13 @@ int cal_b(int *x, int *y, int x1, int y1, int n){
 	return s1/s2;
 }//end
 
-int cal_r(int *x, int *y, int x1, int y1, int n){
-	int r=0,s3=0,s4=0;
+float cal_r(float *x, float *y, float x1, float y1, float n){
+	float r=0,s3=0,s4=0,s5=0;
 	for (int i = 0; i < n; i++)
 	{
 		s3 += (x[i] - x1)*(y[i]-y1);
-		s4 += sqrt(pow((x[i]-x1),2.0) * pow((y[i]-y1),2.0));
+		s4 += pow((x[i]-x1),2.0);
+		s5 += pow((y[i]-y1),2.0);
 	}
-	return s3/s4;
+	return s3/sqrt(s4*s5);
 }//end
